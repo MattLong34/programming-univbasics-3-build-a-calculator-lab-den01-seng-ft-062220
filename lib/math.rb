5321ff05dd380615e1179f00883f817acc4327f5
Matt Long <mdlong34@gmail.com>
2020-05-27 18:47:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  sum
 end
 
 def square_root(num)
